70747bf6fd322b29392fb1aec2ba0d19ccac2a5b
Santiago <santiago.perez01@correo.usa.edu.co>
2021-10-05 18:31:18 -0500

    Added the scrapper function

--- punto1/scrapper/app.py
+++ punto1/scrapper/app.py
@@ -1,24 +1,24 @@
 import json
 import boto3
 import csv
+import ntpath
 from bs4 import BeautifulSoup
 
 def handler(event, context):
     bucketName= event['Records'][0]['s3']['bucket']['name']
     fileName=event['Records'][0]['s3']['object']['key']
+    fileName=fileName.replace('%3D','=')
+    print(bucketName,fileName,flush=True)
     s3 = boto3.resource('s3')
-    s3.meta.client.download_file(bucketName, fileName, '/tmp/'+fileName)
-
-    
-
-    
-    f = open('/tmp/'+fileName,'r',encoding='utf-8')
+    justFileName=ntpath.basename(fileName)   
+    s3.meta.client.download_file(bucketName, fileName, '/tmp/'+justFileName)
+    f = open('/tmp/'+justFileName,'r',encoding='utf-8')
     txt=f.read()
 
     soup = BeautifulSoup(txt,'html.parser')
     articles=soup.find_all('article') #All of these articles, are news
 
-    csvFile = open('/tmp/'+fileName+'.csv', 'w',encoding='utf-8')
+    csvFile = open('/tmp/'+justFileName+'.csv', 'w',encoding='utf-8')
     writer = csv.writer(csvFile,dialect='unix')
     row=['title','section','url']
     writer.writerow(row)
@@ -34,7 +34,7 @@ def handler(event, context):
 
     csvFile.close()
     f.close()
-    s3.meta.client.upload_file('/tmp/'+fileName+'.csv', 'newsdata202110','newspaper/final/'+ fileName+'.csv')
+    s3.meta.client.upload_file('/tmp/'+justFileName+'.csv', 'resultsnewspaperbucket','newspaper/final/'+ fileName+'.csv')
     return {
         'statusCode': 200,
         'body': 'Logs generated!'
